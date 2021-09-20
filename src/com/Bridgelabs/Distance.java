d401485e174ae3c3aae7fb52c89fd4728f59ec43
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-09-20 09:56:11 +0530

    Add:-Find the roots of the equation A*x*x + B*x + C

--- src/com/Bridgelabs/Distance.java
+++ src/com/Bridgelabs/Distance.java
@@ -15,7 +15,7 @@ public class Distance {
 		
 		double distance = Math.sqrt(Math.pow(x,  2) + Math.pow(y, 2));
 		 
-		System.out.println("Distance from (0,0) to (" +x+"," +y+") is" + distance);
+		System.out.println("Eculidean distance from (0,0) to (" +x+"," +y+") is" + distance);
 		
 	}
 
